1e9338b8509197254ba5e861182d73572375df3b
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-20 13:18:22 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -1,95 +1,33 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Kirodewal
-
-# the logging things
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 import os
-import sqlite3
-
-# the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
-
-# the Strings used for this "thing"
 from translation import Translation
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-from helper_funcs.chat_base import TRChatBase
-
-def GetExpiryDate(chat_id):
-    expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
-    Config.AUTH_USERS.add(7351948)
-    return expires_at
-
-
-@pyrogram.Client.on_message(pyrogram.filters.command(["help", "about"]))
-async def help_user(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/help")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.HELP_USER,
-        parse_mode="html",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
-    )
-
-
-@pyrogram.Client.on_message(pyrogram.filters.command(["me"]))
-async def get_me_info(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/me")
-    chat_id = str(update.from_user.id)
-    chat_id, plan_type, expires_at = GetExpiryDate(chat_id)
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.CURENT_PLAN_DETAILS.format(chat_id, plan_type, expires_at),
-        parse_mode="html",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
-    )
-
-@pyrogram.Client.on_message(pyrogram.filters.command(["start"]))
+@Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/start")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.START_TEXT,
-        reply_to_message_id=update.message_id
-    )
-
-
-@pyrogram.Client.on_message(pyrogram.filters.command(["upgrade"]))
-async def upgrade(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/upgrade")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.UPGRADE_TEXT,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
-    )
-
-
-@pyrogram.Client.on_message(pyrogram.filters.command(["source"]))
-async def help_user(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/source")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.SOURCE,
-        parse_mode="html",
+    await update.reply_text(
+        text=Translation.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
+        reply_markup=Translation.START_BUTTONS
+    )
+
+
+@Client.on_message(filters.command(["help"]) & filters.private)
+async def help(bot, update):
+    await update.reply_text(
+        text=Translation.HELP_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=Translation.HELP_BUTTONS
+    )
+
+@Client.on_message(filters.command(["about"]) & filters.private)
+async def about(bot, update):
+    await update.reply_text(
+        text=Translation.ABOUT_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=Translation.ABOUT_BUTTONS
     )
