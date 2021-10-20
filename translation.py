f29e86cafe2a9d8a23a4e42ad9115147e508d0a0
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-20 13:36:15 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -90,8 +90,8 @@ URL | filename | username | password</b>"""
     EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
     UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
     EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice."
-    EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options.
-You can use /rename command after receiving file to rename it with custom thumbnail support."""
+    EXTRACT_ZIP_STEP_TWO = "Select file_name to upload from the below options.
+You can use /rename command after receiving file to rename it with custom thumbnail support."
     CANCEL_STR = "Process Cancelled"
     ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
