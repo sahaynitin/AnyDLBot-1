33916e6732d485bfed2518ebf0d1777cfc2972dc
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-20 13:15:08 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -37,11 +37,30 @@ from hachoir.parser import createParser
 from PIL import Image
 
 
-@pyrogram.Client.on_callback_query()
+@Client.on_callback_query()
 async def button(bot, update):
-    # logger.info(update)
-    cb_data = update.data
-    if "|" in cb_data:
+    if "|" in update.data:
         await youtube_dl_call_back(bot, update)
-    elif "=" in cb_data:
+    elif "=" in update.data:
         await ddl_call_back(bot, update)
+    elif update.data == "home":
+        await update.message.edit_text(
+            text=Translation.START_TEXT.format(update.from_user.mention),
+            reply_markup=Translation.START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=Translation.HELP_TEXT,
+            reply_markup=Translation.HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=Translation.ABOUT_TEXT,
+            reply_markup=Translation.ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+    else:
+        await update.message.delete()
+
