04c73b7ab68b6011fa840c9663e09f6d65c796b8
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-20 13:23:27 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,53 @@
 class Translation(object):
-    START_TEXT = """Hi. 🤠 Thank You for using me .\nIm a simple Telegram All-In-One Bot."""
+    START_TEXT = """
+<b>Hello {}!!!
+
+I'am a url to telegram file or media uploader bot with permanent thumbnail support.</b>
+
+<i>For More Details check Help 📜</i>
+"""
+    HELP_TEXT = """
+<b>Link to Media or File</b>
+- Send a link for upload to telegram file or media.
+
+<b>Set Thumbnail</b>
+- Send a photo to make it as permanent thumbnail.
+
+<b>Deleting Thumbnail</b>
+- Send /delthumb to deleting thumbnail.
+
+<b>Show Thumbnail</b>
+- Send /showthumb to view custom thumbnail.
+"""
+    ABOUT_TEXT = """
+**Bot :** `URL Uploader`
+**Creator :** [Vivek](https://telegram.me/vivektvp)
+**Channel :** [Vk Projects](https://telegram.me/VKPROJECTS)
+**Credits :** `Everyone in this journey`
+**Language :** [Python3](https://python.org)
+**Library :** [Pyrogram v1.2.0](https://pyrogram.org)
+**Server :** [Heroku](https://heroku.com)
+"""
+    START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🤔 Help', callback_data='help'),
+        InlineKeyboardButton('🤖 About', callback_data='about'),
+        ],[
+        InlineKeyboardButton('Close🔐', callback_data='close')
+        ]]
+    )
+    HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🤖 About', callback_data='about'),
+        InlineKeyboardButton('Close 🔐', callback_data='close')
+        ]]
+    )
+    ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏠 Home', callback_data='home'),
+        InlineKeyboardButton('Close 🔐', callback_data='close')
+        ]]
+    )
     RENAME_403_ERR = "<b>Sorry. You Are Not Permitted To Rename This File.</b>"
     ABS_TEXT = " <b>Please Don't Be Selfish.</b>"
     UPGRADE_TEXT = "<b>This Bot Is Free To Use If U R My  Friend.</b>"
